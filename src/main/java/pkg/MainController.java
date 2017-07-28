a64d8882d6c4552a98bfe4c3e8d4a6526bddcefb
Kirby Wirby <kirby900@luckypucky.com>
2017-07-28 07:58:15 -0700

    Added .gitignore

--- src/main/java/pkg/MainController.java
+++ src/main/java/pkg/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class MainController {
 
     @GetMapping("/")
     String index(Authentication auth, Model model) {
